46e5a21181ef47cea38b7d82c5bdddad6fce0cd2
Rafael Santos Teixeira <rafaelsantost87@gmail.com>
2025-09-10 17:06:44 -0300

    settings update

--- pacientes/models.py
+++ pacientes/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 from app.mixins import BaseModel
 
+class Paciente(BaseModel):
+    nome = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=14, unique=True)
+    telefone = models.CharField(max_length=20, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    data_nascimento = models.DateField(null=True, blank=True)
+    observacoes = models.TextField(blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Paciente'
+        verbose_name_plural = 'Pacientes'
+    
+
+    def __str__(self):
+        return self.nome
